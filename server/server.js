ea0ee896507e33062034d951164385dba8408f47
officialbiswasmanpower <official.biswasmanpower@gmail.com>
2026-02-24 15:26:39 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -20,12 +20,14 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected ✅");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT} 🚀`)
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT} 🚀`)
     );
   })
   .catch((err) => console.log(err));
